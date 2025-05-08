f934b7f1f11097789fa1cccb2ef8a564d9f4699e
gryphware <chanhnguyencute12@gmail.com>
2025-05-08 14:26:09 +0700

    remake

--- simple_calculator.c
+++ simple_calculator.c
@@ -1,78 +1,86 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-void getAccumulator(int *accumulator, int number, char operator){
-  switch(operator){
-  case 43: /* add */
-    *accumulator += number;
-    break;
-  case 45: /*subtract*/
-    *accumulator -= number;
-    break;
-  case 42: /*multi*/
-    *accumulator *= number;
-    break;
-  case 47: /*div*/
-    *accumulator /= number;
-    break;
-  }
+int isValid(char *line){
+    while(*line != '\0'){
+        if((*line >= '0' && *line <= '9') || *line == '+' || *line == '-'){
+            line++;
+        } else{
+            return 0;
+        }
+    }
+    return 1;
 }
 
-void getResult(const char *line){
-  
-  int accumulator = 0; /*luu ket qua sau khi xu ly*/
-  char operator = '+'; /*toan tu*/
-  int isInt_Exist = 0;
-  int number = 0;
-  
-  for(size_t i = 0; i < strlen(line); i++){
-    /*co dau = thi dung*/
-    if(line[i] == '='){
-      break;
+int convert(int start, int end, char *line){
+    int result = 0;
+    for(int i = start; i < end; i++){
+        result = result * 10 + (line[i] - '0');
     }
-    /*kiem so*/
-    if(atol(&line[i]) > 0){
-      number = atol(&line[i]);
-      isInt_Exist = 1;
-    }
-    /*kiem operator*/
-    if(line[i] == '+' || line[i] == '-' || line[i] == '*' || line[i] == '/'){
-      operator = line[i];
-    }
-
-    if(isInt_Exist && operator){
-      getAccumulator(&accumulator, number, operator);
-      isInt_Exist = 0;
-      operator = '\0';
-    }
-  }
-
-  printf("%d \n", accumulator);
-  
+    return result;
 }
 
+void calculate(char operator, int num, int *accumulator){
+    switch(operator){
+    case 43:
+        *accumulator += num;
+        break;
+    case 45:
+        *accumulator -= num;
+        break;
+    }
+}
+
+int getResult(char *line){
+    int accumulator = 0; /*ban dau la 0*/
+    int num; /*bien nay nhan so tu ham*/
+    char operator = '+'; /*luu toan tu ban dau la +*/
+
+    int start = 0;
+    size_t len = strlen(line);
+    
+    for(size_t i = 0; i < strlen(line); i++){
+        if(line[i] == '+' || line[i] == '-'){
+            num = convert(start, i, line);
+            start = i + 1;
+            calculate(operator, num, &accumulator);
+            operator = line[i];
+        }
+    }
+
+    num = convert(start, len, line);
+    calculate(operator, num, &accumulator);
+    return accumulator;
+}
 int main(){
 
-  char *line = NULL;
-  size_t len;
-  ssize_t read;
+    /*input formula*/
+    char *line = NULL;
+    size_t len;
+    ssize_t read;
 
-  /*nhap*/
-  read = getline(&line, &len, stdin);
+    read = getline(&line, &len, stdin);
 
-  /*da nhan str an toan?*/
-  if(read < 1){
-    fprintf(stderr, "Read ko doc dc?");
-    free(line);
-    return EXIT_FAILURE;
-  }
+    /*check if input failed*/
+    if(read < 1){
+        fprintf(stderr, "Input bi loi");
+        free(line);
+    }
 
-  line[strcspn(line, "\n")] = 0;
+    /*remove \n if it exist*/
+    line[strcspn(line, "\n")] = 0;
 
-  getResult(line);
-  
-  free(line);
-  
-  return EXIT_SUCCESS;
+    /*a simple formula doesnt have special char like "a" -> "z" and "\", "*"*/
+    int isFormula_Valid = isValid(line);
+    if(!isFormula_Valid){
+        fprintf(stderr, "formula co char dac biet va *, \\ chua ho tro");
+        free(line);
+    }
+
+    int result = getResult(line);
+
+    printf("%d\n", result);
+    
+    return EXIT_SUCCESS;
 }
